7c24c7fe5ec901b18d4b035657627427a6184b21
Rohan Mishra <rohanmishra2017@gmail.com>
2017-12-14 12:36:39 -0800

    Aded url parser

--- analysis.py
+++ analysis.py
@@ -6,6 +6,7 @@ Reads in text file containing formatted history data generated from Google Chrom
 history and creates a pie chart of most popular sites
 """
 
+from urlparse import urlparse
 import pandas as pd
 import numpy as np
 import pprint as p
@@ -16,4 +17,16 @@ with open('hist.txt') as f:
 
 # strip whitespace on first occurance of '|'
 raw_data = [line.split('|', 1) for line in [x.strip() for x in content]]
+
+# dataframe - "datetime" | "url"
+data = pd.DataFrame(raw_data, columns=['datetime', 'url'])
+
+# convert to datetime elements
+data.datetime = pd.to_datetime(data.datetime)
+
+# use urllib.parse to remove info from URL, leaving only domain/subdomain
+parser = lambda u: urlparse(u).netloc
+data.url = data.url.apply(parser)
+
+p.pprint(data.head(1))
 
