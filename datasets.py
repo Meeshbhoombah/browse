a9db2aed011e5b2b8e31983f1fdcb82aa3481881
Rohan Mishra <rohanmishra2017@gmail.com>
2018-02-11 19:59:44 -0800

    Added baseline scraping script

---
+++ datasets.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+"""
+datasets.py
+
+Scrapes browser history from database on disk and converts it into a .csv
+"""
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Browse
+Hidden deep within your filesystem is a database with such power datat
+
